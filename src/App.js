c66fd8bf644bfc18986de83a419cc0952c5b4764
DougRudeen <dougrudeen+git2@gmail.com>
2025-06-21 12:51:17 -0600

    BrowserRouter issue fixed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import LearnMorePage from "./LearnMorePage";
 import BlogPostPage from "./BlogPostPage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/learn-more" element={<LearnMorePage />} />
-        <Route path="/blog/:slug" element={<BlogPostPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/learn-more" element={<LearnMorePage />} />
+      <Route path="/blog/:slug" element={<BlogPostPage />} />
+    </Routes>
   );
 
 }
